add544955654c823c6580ddfbdb4a3c30d789368
Klink <klink@bender>
2010-09-09 16:49:09 +0200

    nouvelle version 47

--- usr/local/bin/stalag13-mplayer-rs.pl
+++ usr/local/bin/stalag13-mplayer-rs.pl
@@ -1,11 +1,44 @@
 #!/usr/bin/perl
+#  Copyright 2010 (c) Mathieu Roy <yeupou--gnu.org> 
+#
+# The Savane project is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# The Savane project is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the Savane project; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
 use strict;
 use POSIX qw/setsid/;
 
-# shut down redshift
-system("killall",
-       "-TERM",
-       "redshift");
+# search for any redshift process in /proc
+my @tokill;
+opendir(PROC, "/proc");
+while (defined(my $pid = readdir(PROC))) {
+    # not a process if not a directory
+    next unless -d "/proc/$pid";
+    # not a process if not containing cmdline
+    next unless -e "/proc/$pid/cmdline";
+    # look out for redshift
+    open(PID, "< /proc/$pid/cmdline");
+    while (<PID>) {
+	# with or without path
+	next unless m/^redshift/ or m/^\/usr\/bin\/redshift/;
+	push(@tokill, $pid);
+    }
+    close(PID);
+}
+closedir(PROC);
+# now kill these with SIGTERM (15), software termination
+kill(15, @tokill);
+
 
 # starts mplayer with given args (we use perl because a basic sh would
 # easily mess up here)
