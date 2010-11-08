75c132dc546734b77edab75fd9527b84e386305b
Klink <klink@bender>
2010-11-08 15:00:48 +0100

    nouvelle version 100

--- usr/local/bin/stalag13-mplayer-rs.pl
+++ usr/local/bin/stalag13-mplayer-rs.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #  Copyright 2010 (c) Mathieu Roy <yeupou--gnu.org> 
 #
-# The Savane project is free software; you can redistribute it and/or
+# Thi program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
