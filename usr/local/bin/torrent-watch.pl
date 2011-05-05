2051aff1391fa55ba587c34bccd29239c68081a8
Klink <klink@bender>
2011-05-05 17:25:16 +0200

    Ready for transmission 2.x

--- usr/local/bin/torrent-watch.pl
+++ usr/local/bin/torrent-watch.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# Copyright (c) 2010 Mathieu Roy <yeupou--gnu.org>
+# Copyright (c) 2010-2011 Mathieu Roy <yeupou--gnu.org>
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -17,33 +17,36 @@
 #   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 #   USA
 
+# Script for transmission 2.x.
+# (for 1.x, the first version of the script is still available on my blog)
+
 use strict "vars";
 use Fcntl ':flock';
 use POSIX qw(strftime);
 use File::Basename;
 
-my $watchdir = "/home/torrent/watch";
-my $bin = "/usr/bin/transmission-remote";
+my $watchdir = "/root/tmprm";
+my $bin = "/usr/bin/transmission-remote"; # Too noisy, so we cannot use system
 my $debug = 0;
 
 # ~/watch syntax :
 #    $file.torrent = torrent to be added
-#    $realfile.hash = being processed (delete it to remove the torrent) 
-#    $realfile.hash- = to be paused
-#    $realfile.hash+ = (supposedly) completed
+#    $realfile.trs = being processed (delete it to remove the torrent) 
+#    $realfile.trs- = to be paused
+#    $realfile.trs+ = (supposedly) completed
 #    all- = pause all 
 
 # check if we are running with torrent user (not with getlogin() because
 # su often mess it up)
-die "This should not be started by ".(getpwuid($<))[0]." but torrent instead. Exit" unless ((getpwuid($<))[0] eq 'torrent');
+#die "This should not be started by ".(getpwuid($<))[0]." but torrent instead. Exit" unless ((getpwuid($<))[0] eq 'torrent');
 
 # enter ~/watch
 chdir($watchdir) or die "Unable to enter $watchdir. Exit";
 
 # silently forbid concurrent runs
 # (http://perl.plover.com/yak/flock/samples/slide006.html)
-open(LOCK, "< $0") or die "Failed to ask lock. Exit";
-flock(LOCK, LOCK_EX | LOCK_NB) or exit;
+#open(LOCK, "< $0") or die "Failed to ask lock. Exit";
+#flock(LOCK, LOCK_EX | LOCK_NB) or exit;
 
 # open log
 open(LOG, ">> $watchdir/log");
@@ -84,6 +87,7 @@ if (-e "$watchdir/.down") {
     unlink("$watchdir/.down");
 }
 
+
 # examine ~/watch
 my $pause_all = 0;
 my $readme_exists = 0;
@@ -108,8 +112,9 @@ while (defined(my $file = readdir(WATCH))) {
 
     # find out suffix, ignore file if none found
     my $suffix = 0;
-    $suffix = $1 if $file =~ /^.*(\.[^.]*)$/;
-    next unless $suffix;
+    my $realfile;
+    if ($file =~ /^(.*)(\.[^.]*)$/) { $suffix = $2; $realfile = $1; }
+    next unless $suffix && $realfile;
     
     # new .torrent file
     if ($suffix eq ".torrent") {
@@ -117,155 +122,124 @@ while (defined(my $file = readdir(WATCH))) {
 	next;
     }
 
-    # if we get here, we have .hash file that contains a hash
-    my $hash;
-    open(HASHFILE, "< $watchdir/$file");
-    while(<HASHFILE>) {
-	# should only contain one line
-	$hash = $_;
-	last;
-    }
-    close(HASHFILE);
-    
-    # marked as being processed
-    if ($suffix eq ".hash") {
-	$marked_as_being_processed{$hash} = $file;
+    # if we get here, we have a .trs file (contains infos about the torrent)
+  
+    # being processed or should be started
+    if ($suffix eq ".trs") {
+	$marked_as_being_processed{$realfile} = 1;
 	next;
     }
     # to be paused
-    if ($suffix eq ".hash-") {
-	$to_be_paused{$hash} = $file;
+    if ($suffix eq ".trs-") {
+	$to_be_paused{$realfile} = 1;
 	next;
     }
 }
 closedir(WATCH);
 
-# update hashes of torrent beings processed,
-#  start/pause/remove if need be
-my %being_processed;
-open(INFO, "$bin --info |");
-while (<INFO>) {
-    # output format: $hash $file
-    my ($hash, $file);
-    if (/^([^\s]*)\s(.*)$/) { $hash = $1; $file = $2; }
-
-    # should be paused
-    if (exists($to_be_paused{$hash})) {
-	print "$bin --stop $hash\n" if $debug;
-	print LOG strftime "%c - pause $file\n", localtime;
-	system($bin,
-	       "--stop",
-	       $hash);
-	next;
-    }
-    
-    # should be removed 
-    unless (-e "$watchdir/$file.hash") {
-	print "$bin --remove $hash\n" if $debug;
-	print LOG strftime "%c - remove $file\n", localtime;
-	system($bin,
-	       "--remove",
-	       $hash);
-	next;
-    }
-
-    # any other case, ask to start it (dont log it, we do it everytime)
-    print "$bin --start $hash\n" if $debug and !$pause_all;
-    system($bin,
-	   "--start",
-	   $hash)
-	unless $pause_all;
-    $being_processed{$hash} = $file;
-}
-close(INFO);
-
 
 # add new torrents
+my %added;
 foreach my $torrent (@to_be_added) {
-    print "$bin --add $watchdir/$torrent\n" if $debug;
-    system($bin,
-	   "--add",
-	   "$watchdir/$torrent");
+    print "$bin --add $watchdir/$torrent --start\n" if $debug;
+    `$bin --add $watchdir/$torrent --start >/dev/null`;
+
+    # get the ID (should be the latest)
+    my $id;
+    open(LIST, "$bin --list |");
+    while (<LIST>) {
+	if (/^\s*(\d*)\*?\s*/) {
+	    $id = $1 if $id < $1;
+	}
+    }
+    $added{$id} = 1;
 }
 unlink(@to_be_added);
-open(INFO, "$bin --info |");
-while (<INFO>) {
-    # output format: $hash $file
-    my ($hash, $file);
-    if (/^([^\s]*)\s(.*)$/) { $hash = $1; $file = $2; }
 
-    # create the hashfile for the newly added torrents
-    next if -e "$watchdir/$file.hash+";
-    next if -e "$watchdir/$file.hash-";
-    next if -e "$watchdir/$file.hash";
-    print "echo $hash > $watchdir/$file.hash\n" if $debug;
-    print LOG strftime "%c - add $file\n", localtime;
-    open(HASHFILE, "> $watchdir/$file.hash");
-    print HASHFILE $hash;
-    close(HASHFILE); 
+# update torrentsbeings processed,
+#  start/pause/remove if need be
+my %being_processed;
+my $count;
+open(LIST, "$bin --list |");
+while (<LIST>) {
 
-    # start them
-    print "$bin --start $hash\n" if $debug and !$pause_all;
-    print LOG strftime "%c - start $file\n", localtime;
-    system($bin,
-	   "--start",
-	   $hash)
-	unless $pause_all;
-    $being_processed{$hash} = $file;
-}
-close(INFO);
+    # output format: 
+    # ID  Done  Have  ETA  Up  Down  Ratio  Status  Name
+    my ($id, $percent, $file);
+    if (/^\s*(\d*)\*?\s*(\d*\%)\s*/) { $id = $1; $percent = $2; }
+    if (/\s*([^\s]*)$/) { $file = $1; }
+    print "ID:$id FILE:$file PERCENT:$percent => $_\n" if $debug;
 
-
-# Update/check status, warn of finished jobs and add README if necessary
-# (fix send mail when finished)
-open(STATUS, "$bin --list |");
-open(STATUSFILE, "> $watchdir/status");
-print STATUSFILE "Last run: ", strftime "%c\n\n", localtime;
-my $count = 0;
-while (<STATUS>) {
-    # check if completed at 100%
-    my ($file, $percent);
-    if (/^(.*)\s\(.*\s.?iB\)\s\-\s(\d*\%)\s/) { $file = $1 ; $percent = $2; } 
-
+    # skip if missing info
+    next unless $id and $file;     
+    
+    # finished
     if ($percent eq "100%") {
 	print "mv $file.hash $file.hash+\n" if $debug;
 	print LOG strftime "%c - completed $file\n", localtime;
-	# do not bother removing the torrent, it will be done 
-	# during next run
-	rename("$watchdir/$file.hash",
-	       "$watchdir/$file.hash+");
+	# do not bother removing the torrent, done below
+	rename("$watchdir/$file.trs",
+	       "$watchdir/$file.trs+");
 	
 	# warn (it should send a mail, if cron is properly configured)
 	print "Hello,\n\nI assume the following torrent was completed:\n\n" 
 	    unless $count;
-	print $_."\n";
+	print "$file (id: $id)\n";
 	$count++;
+
     }
 
-    # updated status file with an extra line break 
-    print STATUSFILE $_."\n";
+
+    # should be paused
+    if (exists($to_be_paused{$file})) {
+	print "$bin -t $id --stop\n" if $debug;
+	print LOG strftime "%c - pause $file\n", localtime;
+	`$bin --torrent $id --stop >/dev/null`;
+	next;
+    }
+    
+    # should be removed 
+    unless (-e "$watchdir/$file.trs" or $added{$id}) {
+	print "$bin -t $id --remove (no $file.trs)\n" if $debug;
+	print LOG strftime "%c - remove $file\n", localtime;
+	`$bin --torrent $id --remove >/dev/null`;
+	next;
+    }
+
+    # any other case, ask to start it (dont log it, we do it everytime)
+    print "$bin -t $id --start\n" if $debug and !$pause_all;
+    `$bin --torrent $id --start >/dev/null` unless $pause_all;
+    $being_processed{$file} = 1;
+
+    # for any processed file, update the info file 
+    open(TRSFILE, "> $watchdir/$file.trs");
+    open(INFO, "$bin --torrent $id --info |");
+    while (<INFO>) {
+	print TRSFILE $_;
+    }
+    close(INFO);
+    close(TRSFILE); 
+
 }
-close(STATUS);
+close(LIST);
+
+# update status info after everything was done
+open(STATUSFILE, "> $watchdir/status");
+print STATUSFILE "Last run: ", strftime "%c\n\n", localtime;
+open(LIST, "$bin --list |");
+while (<LIST>) { print STATUSFILE $_; }
+close(LIST);
+open(STATS, "$bin --session-stats |");
+while (<STATS>) { print STATUSFILE $_; }
+close(STATS);
 close(STATUSFILE);
 
 unless ($readme_exists) {
     open(README, "> $watchdir/README");
-    print README "watch syntax :\n \$file.torrent = to be added\n \$realfile.hash =  being processed (delete it to remove the torrent)\n \$realfile.hash- = to be paused\n \$realfile.hash+ = (supposedly) completed\n all- = pause all\n";
+    print README "watch syntax :\n \$file.torrent = to be added\n \$realfile.trs =  being processed (delete it to remove the torrent)\n \$realfile.trs- = to be paused\n \$realfile.trs+ = (supposedly) completed\n all- = pause all\n";
     close(README);
 }
 
-
-
-
-
-# cleanups
-# (remove hashes of torrents removed by any other mean, etc)
-while (my($hash, $file) = each (%marked_as_being_processed)) {
-    next if exists($being_processed{$hash});
-    print "rm $watchdir/$file\n" if $debug;
-    print LOG strftime "%c - [cleanup] remove $file\n", localtime;
-    unlink("$watchdir/$file");
-}
 
 close(LOG);
 # EOF
