6532c375b717a0f643b9ca886d1d2e6acaa7fd67
Klink <klink@bender>
2011-09-15 18:03:07 +0200

    nouvelle version 183

--- usr/local/bin/send-to-torrent-watch.pl
+++ usr/local/bin/send-to-torrent-watch.pl
@@ -1,4 +1,13 @@
-#!/bin/sh                                                                       
-cd /stockage/torrent/watch
+#!/bin/sh
+# (hum, not .pl!)
+
+# handles heterogenous pathes
+if [ -d /stockage/torrent/watch ]; then
+    cd /stockage/torrent/watch
+fi
+if [ -d /lan/stalag13.ici/torrent-watch ]; then
+    cd /lan/stalag13.ici/torrent-watch
+fi
+
 newname=`echo "$*" | sed "s/[^a-z|A-Z|0-9]//g;"`
 wget -O $newname.torrent "$*"
