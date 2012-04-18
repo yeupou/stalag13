21e0e72e4e2874f5ecfeb18f5b39a37b99141946
Klink <klink@bender>
2012-04-18 08:22:06 +0200

    New release 338

--- usr/local/bin/stalag13-cleanup-music-directory-02-rename.pl
+++ usr/local/bin/stalag13-cleanup-music-directory-02-rename.pl
@@ -82,10 +82,7 @@ while (defined(my $dir = readdir(IMPORT))) {
     die "style = $style; band = $band ; album = $album, exit working $dir " unless ($style and $band and $album);
 
     # various artists case
-    if ($band eq "-----VARIOUS ARTISTS-----") {
-	$is_va = 1;
-	$band = 0;
-    }
+    $is_va if ($band eq "-----VARIOUS ARTISTS-----");
 
     # create the destination directory,
     # FIXME: seems to be some issues with accentued characters, not sure
@@ -149,6 +146,7 @@ while (defined(my $dir = readdir(IMPORT))) {
 		# always extract the correct band name
 		# (yes, not uberclean to call so many times lltag, but let's
 		# keep it stupid/simple)
+		$band = 0;
 		print "Extract BAND from $file (various artists)... ";
 		open(ALBUMINFO, "lltag -S \"$importdir/$dir/$file\" |");
 		while(<ALBUMINFO>) {
