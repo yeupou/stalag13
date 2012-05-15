a777eba3213e9c27277da4caae8aa77bd6d7db0b
Klink <klink@bender>
2012-05-15 09:58:59 +0200

    New release 3.12

--- etc/profile.d/stalag13-shortcuts.sh
+++ etc/profile.d/stalag13-shortcuts.sh
@@ -8,6 +8,18 @@ alias ll='ls -l'
 alias la='ls -A'
 alias l='ls -CF'
 
+# directories
+if [ -d /mnt/lan/gate.stalag13.ici/musique ]; then
+    alias musique='cd /mnt/lan/gate.stalag13.ici/musique'
+fi
+if [ -d /mnt/lan/gate.stalag13.ici/videos ]; then
+    alias videos='cd /mnt/lan/gate.stalag13.ici/videos'
+fi
+if [ -d /mnt/lan/gate.stalag13.ici/suxor ]; then
+    alias suxor='cd /mnt/lan/gate.stalag13.ici/suxor'
+fi
+
+
 # cleanup
 function mrclean { 
     find $1 \( -name "#*#" -or -name ".#*" -or -name "*~" -or -name ".*~" \) -exec rm -rfv {} \; 
