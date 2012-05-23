964d467e61207ae8dceac4d6fe181cf05ea6bbf3
Klink <klink@bender>
2012-05-23 09:28:05 +0200

    New prerelease 1 (on top of 3.15)

--- usr/local/bin/4-2cal.pl
+++ usr/local/bin/4-2cal.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 #
-# Copyright (c) 2011 Mathieu Roy <yeupou--gnu.org>
+# Copyright (c) 2011-2012 Mathieu Roy <yeupou--gnu.org>
+#   http://yeupou.wordpress.com
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -20,6 +21,8 @@
 # requires libdate-calc-perl libcalendar-simple-perl
 
 use strict;
+
+use Getopt::Long;
 use Calendar::Simple;
 use POSIX qw(strftime);
 use Date::Calc qw(Delta_Days);
@@ -27,93 +30,145 @@ use Time::Local;
 use Term::ANSIColor qw(:constants);
 
 my $debug = 0;
+my $month = (localtime)[4] + 1;
+my $year  = (localtime)[5] + 1900;
+my $group  = 3;
+my $full_year = 0;
 
-## get input
 
-my $mon = shift || (localtime)[4] + 1;
-my $yr  = shift || (localtime)[5] + 1900;
-my $group  = shift || 3;
+## get input with standard opts.
+my $getopt;
+eval {
+    $getopt = GetOptions("debug" => \$debug,
+			 "full-year" => \$full_year,
+			 "group=s" => \$group,
+			 "year=s" => \$year,
+			 "month=s" => \$month);
+};
+
+## get input with what remains in ARGS
+for (@ARGV) { 
+    # case month/year
+    if (/^(\d{1,2})\/(\d{4})$/) {
+	$month = $1;
+	$year = $2;
+	last;
+    }
+    # case year/month
+    if (/^(\d{4})\/(\d{1,2})$/) {
+	$year = $1;
+	$month = $2;
+	last;
+    }
+    # one or two numbers: a month
+    if (/^(\d{1,2})$/i) {
+	$month = $1;
+    }
+    # four numbers: a year
+    if (/^(\d{4})$/i) {
+	$year = $1;
+    }      
+}
+
 
 
 ## prepare data
-
-# create a cal output, with month starting on monday (will also check input)
-my @month = calendar($mon, $yr, 1);
-# note for future ref the current day, if we are printing the current month
-my $currentday = 0;
-$currentday = (localtime)[3] if ($mon eq ((localtime)[4] + 1));
-
-# determine time ref depending on the selected group (check input: 1 to 3) 
-# and compare, in days, 1st of currently printed month to it
-my $timedelta = 0;
-$timedelta = Delta_Days(2011, 12, 29, $yr, $mon, 1) if $group eq 1;
-$timedelta = Delta_Days(2011, 12, 25, $yr, $mon, 1) if $group eq 2;
-$timedelta = Delta_Days(2011, 12, 21, $yr, $mon, 1) if $group eq 3;
-die unless $timedelta;
-print "timedelta is $timedelta days\n" if $debug;
-# then determine when exactly we are in the cycle the 1st of currently...
-my $cyclemeridiem = "AMM";
-my $cycleday = 1;
-for (my $day=1; $day <= $timedelta; $day++) {
-    $cycleday++;
-    if ($cycleday > 6) {
-	$cycleday = 1;
-	if ($cyclemeridiem eq "PM") {
-	    $cyclemeridiem = "AM";
-	} else {
-	    $cyclemeridiem = "PM";
-	}
+# month-1 / month / month+1
+my $output_count = 0;
+my $output_max = 3;
+$output_max = 12 if $full_year;
+for ($month = ($month - 1); $output_count < $output_max; $month++) {
+    print "\n" if $output_count > 0;
+    $output_count++;
+    # month is 0, go back a year
+    if ($month < 1) {
+	$year = ($year - 1);
+	$month = 12;
     }
-    print "day $day out of $timedelta:\t$cycleday $cyclemeridiem\n" if $debug;
-}
-print "1st of the month is $cycleday $cyclemeridiem in the group $group cycle\n" if $debug;
-
-
-## print output
-
-# print group
-print "$group/";
-# print i18n month
-print " ", strftime("%B %Y", localtime(timelocal(1,0,0,1,($mon-1),($yr-1900)))), "\n";
-# print i18n week day
-for (my $i=2; $i < 9; $i++) {
-    print substr(strftime("%a", localtime(timelocal(1,0,0,$i,0,112))),
-		 0, 2)." ";
-}
-print "\n";
-foreach (@month) {
-    map { 
-	if ($_) {
-	    # take into account when we are in the cycle
-	    # - output only between 1st and 4th
-	    # - change from AM/PM the 6th and reset
-	    if ($cycleday < 5) {
-		if ($cyclemeridiem eq "AM") {
-		    print WHITE ON_BLUE;
-		} else {
-		    print WHITE ON_MAGENTA;
-		}
+    # month is 13, go forward a year
+    if ($month > 12) {
+	$year = ($year + 1);
+	$month = 1;
+    }
+    
+    # create a cal output, with month starting on monday (will also check input)
+    my @days = calendar($month, $year, 1);
+    # note for future ref the current day, if we are printing the current month
+    my $currentday = 0;
+    $currentday = (localtime)[3] if ($month eq ((localtime)[4] + 1));
+    
+    # determine time ref depending on the selected group (check input: 1 to 3) 
+    # and compare, in days, 1st of currently printed month to it
+    my $timedelta = 0;
+    $timedelta = Delta_Days(2011, 12, 29, $year, $month, 1) if $group eq 1;
+    $timedelta = Delta_Days(2011, 12, 25, $year, $month, 1) if $group eq 2;
+    $timedelta = Delta_Days(2011, 12, 21, $year, $month, 1) if $group eq 3;
+    die unless $timedelta;
+    print "timedelta is $timedelta days\n" if $debug;
+    # then determine when exactly we are in the cycle the 1st of currently...
+    my $cyclemeridiem = "AMM";
+    my $cycleday = 1;
+    for (my $day=1; $day <= $timedelta; $day++) {
+	$cycleday++;
+	if ($cycleday > 6) {
+	    $cycleday = 1;
+	    if ($cyclemeridiem eq "PM") {
+		$cyclemeridiem = "AM";
+	    } else {
+		$cyclemeridiem = "PM";
 	    }
-	    $cycleday++;
-	    if ($cycleday > 6) {
-		$cycleday = 1;		
-		if ($cyclemeridiem eq "AM") {
-		    $cyclemeridiem = "PM";
-		} else {
-		    $cyclemeridiem = "AM";
-		}
-	    }
-
-	    # print in bold current day
-	    print BOLD if ($_ eq $currentday);
-	    print sprintf "%2d ", $_;
-	    
-	    # always reset
-	    print RESET;
-	} else {
-	    print '   '; }
-    } @$_;
+	}
+	print "day $day out of $timedelta:\t$cycleday $cyclemeridiem\n" if $debug;
+    }
+    print "1st of the month is $cycleday $cyclemeridiem in the group $group cycle\n" if $debug;
+    
+    
+    ## print output
+    
+    # print group
+#    print "$group/";
+    # print i18n month
+    print "    ", strftime("%B %Y", localtime(timelocal(1,0,0,1,($month-1),($year-1900)))), "\n";
+    # print i18n week day
+    for (my $i=2; $i < 9; $i++) {
+	print substr(strftime("%a", localtime(timelocal(1,0,0,$i,0,112))),
+		     0, 2)." ";
+    }
     print "\n";
+    foreach (@days) {
+	map { 
+	    if ($_) {
+		# take into account when we are in the cycle
+		# - output only between 1st and 4th
+		# - change from AM/PM the 6th and reset
+		if ($cycleday < 5) {
+		    if ($cyclemeridiem eq "AM") {
+			print WHITE ON_BLUE;
+		    } else {
+			print WHITE ON_MAGENTA;
+		    }
+		}
+		$cycleday++;
+		if ($cycleday > 6) {
+		    $cycleday = 1;		
+		    if ($cyclemeridiem eq "AM") {
+			$cyclemeridiem = "PM";
+		    } else {
+			$cyclemeridiem = "AM";
+		    }
+		}
+		
+		# print in bold current day
+		print BOLD if ($_ eq $currentday);
+		print sprintf "%2d ", $_;
+		
+		# always reset
+		print RESET;
+	    } else {
+		print '   '; }
+	} @$_;
+	print "\n";
+    }
 }
 
 # EOF
