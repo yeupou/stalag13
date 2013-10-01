10e6f41eecab0b7effe67e8111aa4c2033902c41
Mathieu Roy <klink@nibbler.stalag13.ici>
2013-10-01 15:13:19 +0200

    New prerelease 2 (on top of 3.78)

--- usr/local/bin/gateway-switch-access.pl
+++ usr/local/bin/gateway-switch-access.pl
@@ -24,75 +24,174 @@
 # It is a daemon and should be started by an appropriate init script.
 
 
-# store IP when it works so later we wont get in trouble because of temporary
-# DNS failure
-
 # update /var/www/netinfo
 
 use strict;
-use Sys::Hostname;
+use Sys::Syslog;
+use Fcntl ":flock";
+use threads;
+use threads::shared;
 use Getopt::Long;
 use Net::Ping;
+use Socket;
+
+#########  SETUP 
 
 my ($getopt, $save, $help);
+my $rcfile = "/etc/switch-accessrc";
+my $debuglog = 1;
 my $dev_main = "eth0";
 my $dev_backup = "wlan0";
+my @hosts = ("free.fr", "wikipedia.org", "gnu.org");
+my @hosts_override;
+my $delay = 4;
+my $acceptable_failures = 2;
 
+# rcfile if existing
+if (-r $rcfile) {
+    open(RCFILE, "< $rcfile");
+    while(<RCFILE>) {
+	next if /^#/;
+	$dev_main = $1 if /^main-device\s?=\s?(\S*)\s*$/i;
+	$dev_backup = $1 if /^backup-device\s?=\s?(\S*)\s*$/i;
+	$delay = $1 if /^max-delay\s?=\s?(\S*)\s*$/i;
+	$debuglog = 1 if /^debuglog\s*/i;
+	@hosts_override = $1 if /^hosts\s?=\s?(\S*)\s*$/i;
+    }
+    close(RCFILE);
+}
+
+
+# Command line option (override rcfile)
 eval {
     $getopt = GetOptions("help" => \$help,
+			 "delay=n" => \$delay,
+			 "hosts=s" => \@hosts_override,
 			 "main-device=s" => \$dev_main,
 			 "backup-device=s" => \$dev_backup);
 };
 
+# accept a coma-separated list of hosts
+@hosts = split(/,/,join(',',@hosts_override)) if (scalar(@hosts_override));
 
+# Print help
 if ($help) {
     print STDERR <<EOF;
 Usage: $0 [OPTIONS]
 
-    
-      --main-device=eth0     Main internet access device
-      --backup-device=wlan0  Backup internet access device
+Check whether internet access from the main device $dev_main is up 
+against hosts @hosts.
+If none of theses can be reached in a delay of $delay minutes, then switch
+to the backup device $dev_backup.
+   
+      --main-device=ethX     Main internet access device
+      --backup-device=wlanX  Backup internet access device
+      --delay=N              Max delay in minute without connectivity
+      --hosts=domain,domain,...  hosts to be checked against
+                             (comma separated list with no spaces)
+ 
+Alternatively, you can add a $rcfile file containing any option
+on the form:
+
+main-device=eth0
+hosts=thisisp.com,thisdomain.net
+
+(without leading --)
 
 Author: yeupou\@gnu.org
-        http://yeupou.wordpress.com/
+ http://yeupou.wordpress.com/
 EOF
-
-
 exit(1);
 }
 
+############## RUN
 
+# disallow concurrent run
+open(LOCK, "< $0") or die "Failed to ask lock. Exiting";
+flock(LOCK, LOCK_EX | LOCK_NB) or die "Unable to lock. This daemon is already alive. Exiting";
 
+# start logging activity from now on
+openlog("switch-access", "pid", "LOG_DAEMON");
+syslog("info", "started with $dev_main as main and $dev_backup as backup");
 
-## Run
+# assume the network is on when started
+my $dev_main_linkon :shared = 1;
+my $dev_backup_on :shared = 0;
 
+### Autonomous thread to activate the backup device or to deactivate
+# exactly on delay
+async { 
+    while (sleep(($delay * 1))) { # DBG ($delay * 60)
+	syslog("info","DBG main $dev_main_linkon   back $dev_backup_on");
+	if ($dev_main_linkon) {
+	    # Main device on:
+	    # nothing to if backup is not up
+	    next unless $dev_backup_on;
+	    ####SHUTOOWN BACKUP
+	    syslog("info", "$dev_main is back on line, shutting down $dev_backup");
+	} else {
+	    # Main device off:
+	    # nothing to do if we already activated backup device
+	    next if $dev_backup_on;
+	    ###BRING UP BACKUP 
+	    syslog("info", "$dev_main is offline, bringing up $dev_backup");
+	}
+    }
+};
+
+### Main doing connectivity checks every minute (more or less
+# depending on how pings go themselves) on main device
+# Every minute we check the first address. Only if it fails we try the second,
+# and third hosts from the list.
+#
+# Note: if we succesfully pinged an address by name, we save the IP and we'll
+# afterwards only ping the IP. So even if DNS get down, we wont mess with
+# the connectivity. If we fail a ping by IP, then we'll retry by name
+my %hosts_ip;
+my %hosts_linkon;
 
 # check every n seconds if we can connect
-
-# 78.249.90.254 ?
-
-  # ip r
-#default via 78.249.90.254 dev eth1 
-#78.249.90.0/24 dev eth1  proto kernel  scope link  src 78.249.90.43 
-#192.168.1.0/24 dev eth2  proto kernel  scope link  src 192.168.1.1 
-
-08 19:16 klink@bender: ~
-  $ netstat -rn
-Table de routage IP du noyau
-Destination     Passerelle      Genmask         Indic   MSS Fenêtre irtt Iface
-0.0.0.0         192.168.1.1     0.0.0.0         UG        0 0          0 eth2
-
-
-
-
-my $host = "78.249.90.254";
-my $p = Net::Ping->new("udp",5,1,$dev_main);
-if ($p->ping($host)) {
-    print "$host is alive on $dev_main\n";
-} else {
-    print "$host is dead on $dev_main\n";
+while (sleep(60)) {
+    # Ping domains one by one.
+    foreach my $target (@hosts) {
+	# Try to access by IP if registered
+	my $target_ip = $target;
+	$target_ip = $hosts_ip{$target} if exists($hosts_ip{$target});
+	my $p = Net::Ping->new("udp",5,1,$dev_main);
+	my $success = $p->ping($target_ip);
+	$p->close();
+	if ($success) {
+	    # success: record it by incrementing a counter
+	    # until it reach max number of accepted failures
+	    $hosts_linkon{$target}++ 
+		unless $hosts_linkon{$target} >= $acceptable_failures;
+	    # record the IP if not done yet
+	    $hosts_ip{$target} = inet_ntoa(inet_aton($target)) 
+		unless exists($hosts_ip{$target});	    
+	    # log 
+	    syslog("info", "ping ok for $target_ip (".$hosts_linkon{$target}.")") if $debuglog;
+	    # end the loop
+	    last;
+	} else {
+	    # failure: decrement the link on counter
+	    $hosts_linkon{$target}--
+		unless $hosts_linkon{$target} < 1;
+	    # remove the known IP
+	    delete($hosts_ip{$target})
+		if exists($hosts_ip{$target});
+	    # log
+	    syslog("info", "ping failed for $target_ip (".$hosts_linkon{$target}.")");
+	    # continue the loop
+	}
+	
+    }
+    
+    # One host up is enough.
+    $dev_main_linkon = scalar(values %hosts_linkon);
+    syslog("info", "DBG linkon $dev_main_linkon");
 }
-$p->close();
+
+
 
 
 
