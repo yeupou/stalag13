505e0e7ff48de7c7e83eadf5ca487c50e9a4cb68
yeupou <yeupou@gnu.org>
2014-01-09 14:32:58 +0100

    New prerelease 3 (on top of 3.106)

--- usr/local/bin/wakey.pl
+++ usr/local/bin/wakey.pl
@@ -398,6 +398,7 @@ while ($valid_exit < 300 && $word ne $input) {
 
     # at this point, input is validated:
     print GREEN, $input, RESET if $input;
+    print "\n";
 
     # save for later current input
     $previous_input = $input;
