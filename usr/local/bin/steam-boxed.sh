65f1d188dbed4981c5fd5b4418ac7135c3391db5
Klink <klink@bender>
2014-02-17 17:22:24 +0100

    New prerelease 7 (on top of 3.140)

--- usr/local/bin/steam-boxed.sh
+++ usr/local/bin/steam-boxed.sh
@@ -65,8 +65,8 @@ if [ `ls -1 $SESSIONS_DIR/ | wc -l` -lt 1 ]; then
     done
     # dirty hack specific to flash: seems HTML5 now
     #if [ -e $STEAM_ROOT/home/$STEAM_USER/.local/share/Steam/ubuntu12_32 ]; then cp -fv /usr/lib/flashplugin-nonfree/libflashplayer.so $STEAM_ROOT/home/$STEAM_USER/.local/share/Steam/ubuntu12_32; fi
-    # another dirty hack required by steam: STILL NEEDED?
-    #chmod 1777 $STEAM_ROOT/dev/shm
+    # another dirty hack required by steam
+    chmod 1777 $STEAM_ROOT/dev/shm
 else 
     echo -e $YELLOW ==== SKIP SETTING UP SESSION, AT LEAST ONE ALREADY EXISTS ==== $NC    
 fi
