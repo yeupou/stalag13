8e6be801387ba1f964ca13ce6ec1b67466dd0830
Klink <klink@bender>
2015-02-05 13:05:20 +0100

    Remplace SHA1 by SHA2 (new release 3.155)

--- etc/nginx/certs/makecert-sha2.sh
+++ etc/nginx/certs/makecert-sha2.sh
@@ -1,5 +1,5 @@
 KEY=fdqn
 
-openssl req -new -x509 -nodes -sha512 -days 3550 -out $KEY.pem -keyout $KEY.key
+openssl req -newkey rsa:4096 -x509 -nodes -sha512 -days 3550 -out $KEY.pem -keyout $KEY.key
 openssl x509 -in $KEY.pem -outform der -out $KEY.crt
 
