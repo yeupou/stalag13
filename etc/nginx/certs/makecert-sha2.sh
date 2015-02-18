3897763481955ee6bf135ad85889fff3c38512d2
Klink <klink@bender>
2015-02-18 14:43:50 +0100

    New prerelease 4 (on top of 3.163)

--- etc/nginx/certs/makecert-sha2.sh
+++ etc/nginx/certs/makecert-sha2.sh
@@ -1,5 +1,7 @@
 KEY=fdqn
 
+# build up .pem and .key
 openssl req -newkey rsa:4096 -x509 -nodes -sha512 -days 3550 -out $KEY.pem -keyout $KEY.key
+# build .crt (binary der) of the .pem
 openssl x509 -in $KEY.pem -outform der -out $KEY.crt
 
