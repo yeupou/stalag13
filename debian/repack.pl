c97c1dfc9f1b614f4aa845e61821a2e715410757
Klink <klink@bender>
2015-02-21 18:45:18 +0100

    New prerelease 5 (on top of 3.166)

--- debian/repack.pl
+++ debian/repack.pl
@@ -9,7 +9,7 @@ my $main = "utils-ahem";
 my %packages = ("utils" => [],
 		"keyring" => ["/etc/apt"],
                 "cache-apt" = ["/etc/nginx/sites-available/cache-apt", "/etc/cron.weekly/cache-apt"],
-		"cache-steam" = ["/etc/nginx/sites-available/cache-steam", "/etc/cron.daily/cache-steam"]);
+		"cache-steam" = ["/etc/nginx/sites-available/cache-steam", "/etc/cron.daily/cache-steam"],
 		"cache-spoof" = ["/etc/dnsspoof.com", "/etc/default/dnsspoof", "/etc/init.d/dnsspoof"]);
 
 for my $package (keys %packages) {
