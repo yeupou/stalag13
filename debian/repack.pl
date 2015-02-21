341139507209732f930c5b33b9f6551827ab2638
Klink <klink@bender>
2015-02-21 20:30:38 +0100

    New prerelease 30 (on top of 3.166)

--- debian/repack.pl
+++ debian/repack.pl
@@ -6,17 +6,17 @@ my $curdir = $ARGV[0];
 die "Invalid directory passed as argument" unless -d $curdir;
 my $path = "$curdir/debian/stalag13";
 my $main = "utils-ahem";
-my %packages = (utils => ["/etc/bash_completion.d", "/etc/bashrc.d", "/etc/profile.d"],
+my %packages = (utils => ["/etc/bash_completion.d", "/etc/bashrc.d", "/etc/profile.d", "/usr/local/bin/qrename.pl", ],
 		keyring => ["/etc/apt"],
 		"utils-cache-apt" => ["/etc/nginx/sites-available/cache-apt", "/etc/cron.weekly/cache-apt"],
 		"utils-cache-steam" => ["/etc/nginx/sites-available/cache-steam", "/etc/cron.daily/cache-steam"],
 		"utils-cache-spoof" => ["/etc/dnsspoof.conf", "/etc/default/dnsspoof", "/etc/init.d/dnsspoof"],
-		"utils-exim" => ["/etc/exim4", "/usr/local/bin/memcached-exim.pl", "/usr/local/bin/memcached-exim"],
+		"utils-exim" => ["/etc/exim4", "/usr/local/bin/memcached-exim.pl"],
 		"utils-nginx" => ["/etc/init.d/perl-fcgi", "/etc/init.d/php-fcgi", "/etc/nginx/cache_proxy_params", "/etc/nginx/certs", "/etc/nginx/allow_local"],
 		"utils-cloud" => ["/usr/share/owncloud", "/etc/nginx/sites-available/cloud"],
 		"utils-webmail" => ["/usr/share/roundcube", "/var/lib/roundcube/plugins/antibruteforce", "/var/lib/roundcube/plugins/carddav", "/etc/nginx/sites-available/webmail"],
-		"utils-torrent" => ["/usr/local/bin/torrent-watch", "/usr/local/bin/torrent-watch.pl", "/usr/local/bin/torrent-watch", "/etc/cron.d/torrent", "/etc/logrotate.d/torrent"],
-		"utils-tumblr" => ["/usr/local/bin/post-image-to-tumblr-init-auth.pl","/usr/local/bin/post-image-to-tumblr-init-auth", "/usr/local/bin/post-image-to-tumblr", "/usr/local/bin/post-image-to-tumblr.pl", "/usr/local/lib/site_perl/WWW/Tumblr.pm", "/usr/local/lib/site_perl/WWW/Tumblr"]
+		"utils-torrent" => ["/usr/local/bin/torrent-watch", "/usr/local/bin/torrent-watch.pl", "/etc/cron.d/torrent", "/etc/logrotate.d/torrent"],
+		"utils-tumblr" => ["/usr/local/bin/post-image-to-tumblr-init-auth.pl", "/usr/local/bin/post-image-to-tumblr.pl", "/usr/local/lib/site_perl/WWW/Tumblr.pm", "/usr/local/lib/site_perl/WWW/Tumblr"]
     );
 
 for my $package (keys %packages) {
@@ -25,7 +25,18 @@ for my $package (keys %packages) {
 	print "  $_\n";
 	# create parent directory if missing
 	system("/bin/mkdir", "-p", dirname("$path-$package$_")) unless -e dirname("$path-$package$_");
-	# move
+	# move the file
 	system("/bin/mv", "-f", "$path-$main$_", "$path-$package$_");
+	# if it's a script, assume there could be a symlink without ext
+	# to move as well
+	my $symlink =~ s{\.[^.]+$}{};
+	system("/bin/mv", "-f", "$path-$main$symlink", "$path-$package$symlink")
+	    if -l  $symlink;
+	
+	
+	
+
+
+	
     }
 }
