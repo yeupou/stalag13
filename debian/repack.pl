a9a85efba8ddf02c03ae8e2a65b3f3a100b15325
Klink <klink@bender>
2015-02-21 19:42:40 +0100

    New prerelease 17 (on top of 3.166)

--- debian/repack.pl
+++ debian/repack.pl
@@ -6,9 +6,9 @@ my $curdir = $ARGV[0];
 die "Invalid directory passed as argument" unless -d $curdir;
 my $path = "$curdir/debian/stalag13";
 my $main = "utils-ahem";
-my %packages = (utils => [["/etc/bash_completion.d", "/etc/bashrc.d", "/etc/profile.d"],
+my %packages = (utils => ["/etc/bash_completion.d", "/etc/bashrc.d", "/etc/profile.d"],
 		keyring => ["/etc/apt"],
-                "utils-cache-apt" => ["/etc/nginx/sites-available/cache-apt", "/etc/cron.weekly/cache-apt"],
+		"utils-cache-apt" => ["/etc/nginx/sites-available/cache-apt", "/etc/cron.weekly/cache-apt"],
 		"utils-cache-steam" => ["/etc/nginx/sites-available/cache-steam", "/etc/cron.daily/cache-steam"],
 		"utils-cache-spoof" => ["/etc/dnsspoof.conf", "/etc/default/dnsspoof", "/etc/init.d/dnsspoof"],
 		"utils-nginx" => ["/etc/init.d/perl-fcgi", "/etc/init.d/php-fcgi", "/etc/nginx/cache_proxy_params", "/etc/nginx/certs", "/etc/nginx/allow_local"],
