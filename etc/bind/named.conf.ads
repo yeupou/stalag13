07f2d215e4a2d3eb54e1f91a8f8829f82971f16c
Klink <klink@bender>
2015-07-29 16:56:02 +0200

    New prerelease 8 (on top of 3.222)

---
+++ etc/bind/named.conf.ads
@@ -0,0 +1,4 @@
+// this can be used to ban ads servers content from generating trafic on you network
+// add a symlink to /usr/local/bin/update-bind-ads-block.pl in /etc/cron.weekly if
+// you want to use it.
+// It will overwrite this file.
